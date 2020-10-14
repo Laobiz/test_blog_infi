4ac8e4e69862c9da569ac7235faf587663d4d405
laobiz <pothjonas@gmail.com>
2020-10-14 11:36:07 +0200

    initial

---
+++ build/Generated/Types.hs
@@ -0,0 +1,31 @@
+-- This file is auto generated and will be overriden regulary. Please edit `Application/Schema.hs` to customize the Types
+{-# LANGUAGE TypeSynonymInstances, FlexibleInstances, InstanceSigs, MultiParamTypeClasses, TypeFamilies, DataKinds, TypeOperators, UndecidableInstances, ConstraintKinds, StandaloneDeriving  #-}
+module Generated.Types where
+
+import IHP.HaskellSupport
+import IHP.ModelSupport
+import CorePrelude hiding (id) 
+import Data.Time.Clock 
+import qualified Data.Time.Calendar
+import qualified Data.List as List 
+import qualified Data.ByteString as ByteString 
+import Database.PostgreSQL.Simple
+import Database.PostgreSQL.Simple.FromRow
+import Database.PostgreSQL.Simple.FromField hiding (Field, name)
+import Database.PostgreSQL.Simple.ToField hiding (Field)
+import qualified IHP.Controller.Param
+import GHC.TypeLits
+import Data.UUID (UUID)
+import Data.Default
+import qualified IHP.QueryBuilder as QueryBuilder
+import qualified Data.Proxy
+import GHC.Records
+import Data.Data
+import qualified Data.String.Conversions
+import qualified Data.Text.Encoding
+import qualified Data.Aeson
+import Database.PostgreSQL.Simple.Types (Query (Query), Binary ( .. ))
+import qualified Database.PostgreSQL.Simple.Types
+
+
+
